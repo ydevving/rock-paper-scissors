382e03b2f4a9d17766e6758c834d91d2cfb64897
y <y@DESKTOP-0FE28PG>
2023-10-01 18:37:51 +0200

    RPS Random Choice function

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
+
+function getComputerChoice() {
+    let number = randomNumber(1, 4);
+    return number == 1 ? "Rock"
+        : number == 2 ? "Paper"
+        : "Scissors"
+}
