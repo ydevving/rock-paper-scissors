8d91bd61cf84c97edff9a19e36335a8e2eaba56f
y <y@DESKTOP-0FE28PG>
2023-10-02 17:12:19 +0200

    playRound() bug fix & game() function

--- main.js
+++ main.js
@@ -11,13 +11,26 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
 
     if (playerSelection == computerSelection) return "Draw";
-    else if (playerSelection == "rock" && computerSelection == "Paper") return "You Lose! Paper beats Rock";
-    else if (playerSelection == "rock" && computerSelection == "Scissors") return "You Win! Rock beats Scissors";
-    else if (playerSelection == "paper" && computerSelection == "Rock") return "You Win! Paper beats Rock";
-    else if (playerSelection == "paper" && computerSelection == "Scissors") return "You Lose! Scissors beats Paper";
-    else if (playerSelection == "scissors" && computerSelection == "Rock") return "You Lose! Rock beats Scissors";
-    else if (playerSelection == "scissors" && computerSelection == "Paper") return "You Win! Scissors cuts paper"
+    else if (playerSelection == "rock" && computerSelection == "paper") return "You Lose! Paper beats Rock";
+    else if (playerSelection == "rock" && computerSelection == "scissors") return "You Win! Rock beats Scissors";
+    else if (playerSelection == "paper" && computerSelection == "rock") return "You Win! Paper beats Rock";
+    else if (playerSelection == "paper" && computerSelection == "scissors") return "You Lose! Scissors beats Paper";
+    else if (playerSelection == "scissors" && computerSelection == "rock") return "You Lose! Rock beats Scissors";
+    else if (playerSelection == "scissors" && computerSelection == "paper") return "You Win! Scissors cuts paper";
     else return "Something has gone wrong";
 }
+
+function game() {
+    for (let round = 0; round < 5; round++) {
+        let player_choice = prompt("Choose 'Rock', 'Paper' or 'Scissors'");
+        let computer_choice = getComputerChoice();
+        console.log(player_choice, computer_choice);
+        let result = playRound(player_choice, computer_choice);
+        console.log(result);
+    }
+}
+
+game()
