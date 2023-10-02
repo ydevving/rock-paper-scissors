fb55a752494e25bcf1ef3145e037ac1ab7c63951
y <y@DESKTOP-0FE28PG>
2023-10-02 16:35:32 +0200

    Function for playing a round

--- main.js
+++ main.js
@@ -6,5 +6,18 @@ function getComputerChoice() {
     let number = randomNumber(1, 4);
     return number == 1 ? "Rock"
         : number == 2 ? "Paper"
-        : "Scissors"
+        : "Scissors";
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection == computerSelection) return "Draw";
+    else if (playerSelection == "rock" && computerSelection == "Paper") return "You Lose! Paper beats Rock";
+    else if (playerSelection == "rock" && computerSelection == "Scissors") return "You Win! Rock beats Scissors";
+    else if (playerSelection == "paper" && computerSelection == "Rock") return "You Lose! Paper beats Rock";
+    else if (playerSelection == "paper" && computerSelection == "Scissors") return "You Win! Paper beats Scissors";
+    else if (playerSelection == "scissors" && computerSelection == "Rock") return "You Lose! Rock beats Scissors";
+    else if (playerSelection == "scissors" && computerSelection == "Paper") return "You Win! Scissors cuts paper"
+    else return "Something has gone wrong";
 }
