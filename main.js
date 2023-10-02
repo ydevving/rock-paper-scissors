b4d3c2d16b883d21133694c96bff7e82b2636d5b
y <y@DESKTOP-0FE28PG>
2023-10-02 16:41:29 +0200

    playRound() bug fix - Paper won to scissors

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) return "Draw";
     else if (playerSelection == "rock" && computerSelection == "Paper") return "You Lose! Paper beats Rock";
     else if (playerSelection == "rock" && computerSelection == "Scissors") return "You Win! Rock beats Scissors";
-    else if (playerSelection == "paper" && computerSelection == "Rock") return "You Lose! Paper beats Rock";
-    else if (playerSelection == "paper" && computerSelection == "Scissors") return "You Win! Paper beats Scissors";
+    else if (playerSelection == "paper" && computerSelection == "Rock") return "You Win! Paper beats Rock";
+    else if (playerSelection == "paper" && computerSelection == "Scissors") return "You Lose! Scissors beats Paper";
     else if (playerSelection == "scissors" && computerSelection == "Rock") return "You Lose! Rock beats Scissors";
     else if (playerSelection == "scissors" && computerSelection == "Paper") return "You Win! Scissors cuts paper"
     else return "Something has gone wrong";
